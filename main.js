ffe25afd53b5808084e7fccc98a5f3293b518529
Jordan D'Souza <jordandsouza352@gmail.com>
2023-01-16 23:28:01 +1300

    removed console logs

--- main.js
+++ main.js
@@ -3,11 +3,9 @@ console.log(document.querySelector(".rating-modal__thanks"))
 
 // Form submit button event listener
 document.querySelector(".rating-modal__rating").addEventListener("click", (e)=>{
-  console.log(e.target)
   if(e.target.tagName === "INPUT"){
     rating.innerText = e.target.value;
   }else if(e.target.tagName === "BUTTON"){
-    console.log(rating.innerText)
     e.preventDefault();
     if(rating.innerText){
       document.querySelector(".rating-modal__default").style.display = "none";

e596b3bc0a9e6e366548bc24d1685ace3fdf6417
Jordan D'Souza <jordandsouza352@gmail.com>
2023-01-16 18:56:24 +1300

    tried to add js for the submit button

---
+++ main.js
@@ -0,0 +1,17 @@
+rating = document.querySelector(".rating")
+console.log(document.querySelector(".rating-modal__thanks"))
+
+// Form submit button event listener
+document.querySelector(".rating-modal__rating").addEventListener("click", (e)=>{
+  console.log(e.target)
+  if(e.target.tagName === "INPUT"){
+    rating.innerText = e.target.value;
+  }else if(e.target.tagName === "BUTTON"){
+    console.log(rating.innerText)
+    e.preventDefault();
+    if(rating.innerText){
+      document.querySelector(".rating-modal__default").setAttribute("id", "display-none")
+      document.querySelector(".rating-modal__thanks").removeAttribute("id", "display-none")
+    }
+  }
+})
